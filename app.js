447b1e8ce11348cc8618c56b8a890029098bba05
lordisrael <josephisrael206@gmail.com>
2023-06-20 15:15:51 +0100

    Correction of some errors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('express-async-errors')
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "students",
-    password: "loveiman",
-    port: 5432
+    user: process.env.POSTGRES_USERNAME,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT
 })
 
 module.exports = pool
